f90d2fe3f675edcd526ea9530fb417a245698e1c
Belford <your email address>
2024-01-22 12:22:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Hello_World from "./components/helloworld";
+function App(){
+  return(
+  <Hello_World />
+)
+}
+ 
+export default App
---
+++ src/components/helloworld.jsx
@@ -0,0 +1,10 @@
+// import react from "react" ; 
+function Hello_World()
+{
+    return(
+        <div>
+          <h1>Hello World</h1>
+        </div>
+    );
+}
+ export default Hello_World;
